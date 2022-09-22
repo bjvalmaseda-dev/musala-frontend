75e3d5dafb041af29c941fdfc4b8efd0a75a63a8
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-09-22 15:21:26 -0400

    :construction: [ADD] progress bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,15 @@
 import Layout from '@layout/Layout';
 import GlobalContextProvider from 'contexts/GlobalContext';
 
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+//Route Events.
+NProgress.configure({ showSpinner: false });
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 function MyApp({ Component, pageProps }) {
   return (
     <GlobalContextProvider>
